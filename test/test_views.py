2cb4e5e5f9c9ed3ce7ffee565128169854409d05
Loic NICOLAS <loic.ln@gmail.com>
2018-07-11 14:30:09 +0200

    handle get 1 product by id

---
+++ test/test_views.py
@@ -0,0 +1,22 @@
+# tests/test_views.py
+from flask_testing import TestCase
+from wsgi import app
+
+class TestViews(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def test_products_json(self):
+        response = self.client.get("/api/v1/products")
+        products = response.json
+        self.assertIsInstance(products, list)
+        self.assertGreater(len(products), 3) # 3 is not a mistake here.
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_products_id1_json(self):
+        response = self.client.get("/api/v1/products/1")
+        product = response.json
+        self.assertIsInstance(product, object)
+        self.assertEqual(response.status_code, 200)
--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,54 @@
 # wsgi.py
 from flask import Flask
 from flask import jsonify
+from flask import abort
+from flask import Response
 app = Flask(__name__)
 
-@app.route('/')
+
+the_products = [
+    {'id': 1, 'name': 'Skello'},
+    {'id': 2, 'name': 'Socialive.tv'},
+    {'id': 3, 'name': 'plop'},
+    {'id': 4, 'name': 'bidule'}
+]
+
+the_products_dic = {
+    1: {'id': 1, 'name': 'Skello'},
+    2: {'id': 2, 'name': 'Socialive.tv'},
+    3: {'id': 3, 'name': 'plop'},
+    4: {'id': 4, 'name': 'bidule'}
+}
+
+
+@app.route('/', methods=['GET'])
 def hello():
     return "Hello  !!!"
 
-@app.route('/api/v1/products')
+
+@app.route('/api/v1/products', methods=['GET'])
 def getProducts():
-    the_products = [
-        { 'id': 1, 'name': 'Skello' },
-        { 'id': 2, 'name': 'Socialive.tv' }
-    ]
-    return jsonify(the_products)
+    return jsonify(the_products), 200
+
+
+@app.route('/api/v1/products/<int:id>', methods=['GET'])
+def getAProduct(id):
+    if id in the_products_dic:
+        return jsonify(the_products_dic[id]), 200
+    else:
+        abort(Response(f"product id:{id} not found ", 404) )
+
+
+@app.route('/api/v1/products/:id', methods=['DELETE'])
+def delProducts():
+    return None, 500
+
+
+@app.route('/api/v1/products', methods=['POST'])
+def createProducts():
+    return None, 500
+
+
+@app.route('/api/v1/products/:id', methods=['PATCH'])
+def updateProducts():
+    return None, 500
