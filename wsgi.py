de0a2ed1f78a8899bb50035832c50f625a9a870c
Loic NICOLAS <loic.ln@gmail.com>
2018-07-11 11:33:26 +0200

    deploy it

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,16 @@
 # wsgi.py
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello  !!!"
+
+@app.route('/api/v1/products')
+def getProducts():
+    the_products = [
+        { 'id': 1, 'name': 'Skello' },
+        { 'id': 2, 'name': 'Socialive.tv' }
+    ]
+    return jsonify(the_products)
